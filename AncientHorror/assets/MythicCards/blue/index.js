a969530399840a26f889151395495382f1c4c600
TheSirsh <sirsh@bk.ru>
2022-09-06 15:49:35 +0300

    new: add project Ancient Horror

---
+++ AncientHorror/assets/MythicCards/blue/index.js
@@ -0,0 +1,63 @@
+const cardsBlue = [{
+  "cardTag" : "blue3",
+  "cardName": "Растущее безумие",
+  "type"    : "snowflake",
+  "imgSrc"  : './assets/MythicCards/blue/blue3.png'
+},{
+  "cardTag" : "blue4",
+  "cardName": "Тайны прошлого",
+  "type"    : "snowflake",
+  "imgSrc"  : './assets/MythicCards/blue/blue4.png'
+},{
+  "cardTag" : "blue5",
+  "cardName": "Расколотая реальность",
+  "type"    : "snowflake",
+  "imgSrc"  : './assets/MythicCards/blue/blue5.png'
+},{
+  "cardTag" : "blue10",
+  "cardName": "Утраченное знание",
+  "type"    : "snowflake",
+  "imgSrc"  : './assets/MythicCards/blue/blue10.png'
+},{
+  "cardTag" : "blue7",
+  "cardName": "Возвращение древних",
+  "type"    : "normal",
+  "imgSrc"  : './assets/MythicCards/blue/blue7.png'
+},{
+  "cardTag" : "blue9",
+  "cardName": "Стёртый из общества",
+  "type"    : "normal",
+  "imgSrc"  : './assets/MythicCards/blue/blue9.png'
+},{
+  "cardTag" : "blue11",
+  "cardName": "Звёзды выстроились",
+  "type"    : "normal",
+  "imgSrc"   : './assets/MythicCards/blue/blue11.png'
+},{
+  "cardTag" : "blue12",
+  "cardName": "Оседлавший ветер",
+  "type"    : "normal",
+  "imgSrc"  : './assets/MythicCards/blue/blue12.png'
+},{
+  "cardTag" : "blue1",
+  "cardName": "Таинственное сияние",
+  "type"    : "tentacles",
+  "imgSrc"  : './assets/MythicCards/blue/blue1.png'
+},{
+  "cardTag" : "blue2",
+  "cardName": "Паутина между мирами",
+  "type"    : "tentacles",
+  "imgSrc"  : './assets/MythicCards/blue/blue2.png'
+},{
+  "cardTag" : "blue6",
+  "cardName": "Эпидемия",
+  "type"    : "tentacles",
+  "imgSrc"  : './assets/MythicCards/blue/blue6.png'
+},{
+  "cardTag" : "blue8",
+  "cardName": "Миры сталкиваются",
+  "type"    : "tentacles",
+  "imgSrc"  : './assets/MythicCards/blue/blue8.png'
+}]
+
+export default cardsBlue
---
+++ AncientHorror/assets/MythicCards/green/index.js
@@ -0,0 +1,93 @@
+const cardsGreen = [{
+  "cardTag" : "green1",
+  "cardName": "Предложение",
+  "type"    : "snowflake",
+  "imgSrc"  : './assets/MythicCards/green/green1.png'
+},{
+  "cardTag" : "green12",
+  "cardName": "То, что гложет",
+  "type"    : "snowflake",
+  "imgSrc"  : './assets/MythicCards/green/green12.png'
+},{
+  "cardTag" : "green16",
+  "cardName": "Гнездо порока",
+  "type"    : "snowflake",
+  "imgSrc"  : './assets/MythicCards/green/green16.png'
+},{
+  "cardTag" : "green17",
+  "cardName": "Покупка информации",
+  "type"    : "snowflake",
+  "imgSrc"  : './assets/MythicCards/green/green17.png'
+},{
+  "cardTag" : "green18",
+  "cardName": "Поддержка церкви",
+  "type"    : "snowflake",
+  "imgSrc"  : './assets/MythicCards/green/green18.png'
+},{
+  "cardTag" : "green7",
+  "cardName": "Призыв Старцев",
+  "type"    : "normal",
+  "imgSrc"  : './assets/MythicCards/green/green7.png'
+},{
+  "cardTag" : "green8",
+  "cardName": "Банковские услуги",
+  "type"    : "normal",
+  "imgSrc"  : './assets/MythicCards/green/green8.png'
+},{
+  "cardTag" : "green9",
+  "cardName": "Обнищание",
+  "type"    : "normal",
+  "imgSrc"  : './assets/MythicCards/green/green9.png'
+},{
+  "cardTag" : "green10",
+  "cardName": "Неожиданное предательство",
+  "type"    : "normal",
+  "imgSrc"  : './assets/MythicCards/green/green10.png'
+},{
+  "cardTag" : "green11",
+  "cardName": "Реки крови",
+  "type"    : "normal",
+  "imgSrc"  : './assets/MythicCards/green/green11.png'
+},{
+  "cardTag" : "green13",
+  "cardName": "Охрана границ",
+  "type"    : "normal",
+  "imgSrc"  : './assets/MythicCards/green/green13.png'
+},{
+  "cardTag" : "green14",
+  "cardName": "Пространственная нестабильность",
+  "type"    : "normal",
+  "imgSrc"  : './assets/MythicCards/green/green14.png'
+},{
+  "cardTag" : "green15",
+  "cardName": "Нет покоя павшим",
+  "type"    : "normal",
+  "imgSrc"  : './assets/MythicCards/green/green15.png'
+},{
+  "cardTag" : "green2",
+  "cardName": "Разорванный на куски",
+  "type"    : "tentacles",
+  "imgSrc"  : './assets/MythicCards/green/green2.png'
+},{
+  "cardTag" : "green3",
+  "cardName": "Под наблюдением",
+  "type"    : "tentacles",
+  "imgSrc"  : './assets/MythicCards/green/green3.png'
+},{
+  "cardTag" : "green4",
+  "cardName": "Нарустающий ужас",
+  "type"    : "tentacles",
+  "imgSrc"  : './assets/MythicCards/green/green4.png'
+},{
+  "cardTag" : "green5",
+  "cardName": "Извне",
+  "type"    : "tentacles",
+  "imgSrc"  : './assets/MythicCards/green/green5.png'
+},{
+  "cardTag" : "green6",
+  "cardName": "Связанный тёмной волей",
+  "type"    : "tentacles",
+  "imgSrc"  : './assets/MythicCards/green/green6.png'
+}]
+
+export default cardsGreen;
